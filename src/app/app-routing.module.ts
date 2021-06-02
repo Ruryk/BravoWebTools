c288270164f63659a76750a2aa5c83096088e350
Vlad Ivashchenko <mr.ruryks@gmail.com>
2021-06-02 16:20:46 +0300

    add material components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('src/app/shared/main/main.module').then(m => m.MainModule) },
   { path: 'login', loadChildren: () => import('src/app/shared/login/login.module').then(m => m.LoginModule) },
-  { path: 'login-code', loadChildren: () => import('src/app/shared/login/login.module').then(m => m.LoginModule) },
+  { path: 'login-code', loadChildren: () => import('src/app/shared/login-code/login-code.module').then(m => m.LoginCodeModule) },
   // { path: '**', loadChildren: () => import('src/app/shared/error/error.module').then(m => m.ErrorModule) }
 ];
 
--- src/app/shared/material-ui/material-ui.module.ts
+++ src/app/shared/material-ui/material-ui.module.ts
@@ -1,10 +1,63 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCommonModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatCommonModule,
+    MatDialogModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatExpansionModule,
+    MatAutocompleteModule
+  ],
+  exports: [
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatCommonModule,
+    MatDialogModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatExpansionModule,
+    MatAutocompleteModule
   ]
 })
-export class MaterialUiModule { }
+export class MaterialUiModule {
+}
