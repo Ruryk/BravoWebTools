d6a182fb3112f7b1781e59700cfb71902d748dd7
Vlad Ivashchenko <mr.ruryks@gmail.com>
2021-06-02 15:29:35 +0300

    add shared.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
+// const routes: Routes = [
+//   { path: '', loadChildren: () => import('src/app/shared/main/main.module').then(m => m.MainModule) },
+//   // {path: '', loadChildren: () => import('src/app/shared/main/main.module').then(m => m.MainModule), canActivate: [AuthGuard]},
+//   { path: 'login', loadChildren: () => import('src/app/shared/login/login.module').then(m => m.LoginModule) },
+//   { path: 'registration', loadChildren: () => import('src/app/shared/registration/registration.module').then(m => m.RegistrationModule) },
+//   { path: '**', loadChildren: () => import('src/app/shared/error/error.module').then(m => m.ErrorModule) }
+// ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {StoreModule} from '@ngrx/store';
+import {reducers, metaReducers} from './reducers';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+import {SharedModule} from './shared/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +18,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
