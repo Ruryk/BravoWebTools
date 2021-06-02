59cd915356fe81f134a6b8ba5be4b17d8d0c118b
Vlad Ivashchenko <mr.ruryks@gmail.com>
2021-06-02 15:35:36 +0300

    add login && login-code.module

--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MaterialUiModule} from '../material-ui/material-ui.module';
+import {LoginModule} from '../login/login.module';
+import {LoginCodeModule} from '../login-code/login-code.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialUiModule,
+    LoginModule,
+    LoginCodeModule
   ],
   exports: [
-    MaterialUiModule
+    MaterialUiModule,
+    LoginModule,
+    LoginCodeModule
   ]
 })
 export class SharedModule { }
