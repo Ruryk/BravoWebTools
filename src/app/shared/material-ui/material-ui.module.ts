737a9432a30c98fb03edb312298427c4f0dff578
Vlad Ivashchenko <mr.ruryks@gmail.com>
2021-06-02 15:32:04 +0300

    add material-ui.module

--- src/app/shared/material-ui/material-ui.module.ts
+++ src/app/shared/material-ui/material-ui.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MaterialUiModule} from '../material-ui/material-ui.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ],
-  exports: [
-    MaterialUiModule
   ]
 })
-export class SharedModule { }
+export class MaterialUiModule { }
