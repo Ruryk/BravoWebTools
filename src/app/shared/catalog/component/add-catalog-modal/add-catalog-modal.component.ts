e6828961a51a2902a8b775c7eea1078a3e550336
Vlad Ivashchenko <mr.ruryks@gmail.com>
2021-06-17 18:33:55 +0300

    add button "Add Unit" in catalog modal "Add Catalog"

--- src/app/shared/catalog/component/add-catalog-modal/add-catalog-modal.component.ts
+++ src/app/shared/catalog/component/add-catalog-modal/add-catalog-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,  Validators} from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-catalog-modal',
@@ -7,6 +7,7 @@ import { FormControl,  Validators} from '@angular/forms';
   styleUrls: ['./add-catalog-modal.component.scss']
 })
 export class AddCatalogModalComponent implements OnInit {
+  public unit: Array<object>;
   public foods: any = [
     { value: 'In stock', viewValue: 'In stock' },
     { value: 'Out of stock', viewValue: 'Out of stock' },
@@ -15,9 +16,23 @@ export class AddCatalogModalComponent implements OnInit {
   public selected = new FormControl('In stock');
 
   constructor() {
+    this.unit = [{
+      unit: '',
+      price: ''
+    }];
   }
 
   ngOnInit(): void {
   }
 
+  addUnit(): void {
+    this.unit.push({
+      unit: '',
+      price: ''
+    });
+  }
+
+  deleteUnit(id: number): void {
+    this.unit.splice(id, 1);
+  }
 }
